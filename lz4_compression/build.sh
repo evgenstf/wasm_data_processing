f00d93cc80c29dcef33695979ce807e995b70b37
Evgenii Kazakov <evgenstf@gmail.com>
2020-09-27 22:02:17 +0000

    Optimize flags in build script

--- lz4_compression/build.sh
+++ lz4_compression/build.sh
@@ -1,2 +1,1 @@
-em++ main.cpp -O2 lz4.c -o wasm_compressor.js -s EXPORTED_FUNCTIONS='["_compress_data","_decompress_data"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' -s FORCE_FILESYSTEM=1 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1
-
+em++ main.cpp lz4.c -o wasm_compressor.js -s EXPORTED_FUNCTIONS='["_compress_data","_decompress_data"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -O2
