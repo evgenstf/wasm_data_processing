b0f0de1e4b3976f2bdaf32765920ab7b01611ca4
Evgenii Kazakov <evgenstf@gmail.com>
2020-09-28 12:16:56 +0300

    Refactor gcd sample

--- gcd_sample/main.cpp
+++ gcd_sample/main.cpp
@@ -2,14 +2,11 @@
 
 
 extern "C" {
-
 int calculate_gcd(int a, int b) {
   while (a != 0 && b != 0) {
     a %= b;
     std::swap(a, b);
   }
-  auto result = a + b;
-  return result;
+  return a + b;
 }
-
 }
